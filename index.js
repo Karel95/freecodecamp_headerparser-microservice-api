9cfdd7a539cd7370719b3e8a5b8ea93a6151cefd
AethiQs-Karel <karel.hernandez@aethiqs.nl>
2025-03-31 15:13:20 -0300

    Refactor /api/whoami endpoint to restore functionality and improve code structure

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
-app.get("/api/whoami", function (req, res) {
-  const ip = req.ip;
-  // console.log(JSON.stringify(req.headers));
-  const language = req.headers['accept-language'];
-  const software = req.headers['user-agent'];
-  res.send({ip: ip, language: language, software: software});
-})
-
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
@@ -28,8 +20,12 @@ app.get("/", function (req, res) {
 });
 
 // your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  const ip = req.ip;
+  // console.log(JSON.stringify(req.headers));
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  res.send({ip: ip, language: language, software: software});
 });
 
 // listen for requests :)
